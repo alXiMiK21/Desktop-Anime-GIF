b3f3ad6c70bec72dc9a52e7a4390068e72a646ad
alXiMiK21 <artema2431@mail.ru>
2025-04-11 19:29:43 +0300

    Initial commit

---
+++ Forms/Menu.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace DesktopKonata.Forms
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            menuStrip2 = new MenuStrip();
+            btn_GIF1 = new ToolStripMenuItem();
+            btn_GIF2 = new ToolStripMenuItem();
+            btn_GIF3 = new ToolStripMenuItem();
+            btn_GIF4 = new ToolStripMenuItem();
+            btn_GIF5 = new ToolStripMenuItem();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            menuStrip2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            menuStrip2.Items.AddRange(new ToolStripItem[] { btn_GIF1, btn_GIF2, btn_GIF3, btn_GIF4, btn_GIF5 });
+            menuStrip2.Location = new Point(0, 0);
+            menuStrip2.Name = "menuStrip2";
+            menuStrip2.Size = new Size(249, 24);
+            menuStrip2.TabIndex = 1;
+            menuStrip2.Text = "menuStrip2";
+            menuStrip2.ItemClicked += menuStrip2_ItemClicked;
+            // 
+            // btn_GIF1
+            // 
+            btn_GIF1.Name = "btn_GIF1";
+            btn_GIF1.Size = new Size(42, 20);
+            btn_GIF1.Text = "1.gif";
+            btn_GIF1.Click += btn_gif1_Click;
+            // 
+            // btn_GIF2
+            // 
+            btn_GIF2.Name = "btn_GIF2";
+            btn_GIF2.Size = new Size(42, 20);
+            btn_GIF2.Text = "2.gif";
+            btn_GIF2.Click += btn_gif2_Click;
+            // 
+            // btn_GIF3
+            // 
+            btn_GIF3.Name = "btn_GIF3";
+            btn_GIF3.Size = new Size(42, 20);
+            btn_GIF3.Text = "3.gif";
+            btn_GIF3.Click += btn_gif3_Click;
+            // 
+            // btn_GIF4
+            // 
+            btn_GIF4.Name = "btn_GIF4";
+            btn_GIF4.Size = new Size(42, 20);
+            btn_GIF4.Text = "4.gif";
+            btn_GIF4.Click += btn_gif4_Click;
+            // 
+            // btn_GIF5
+            // 
+            btn_GIF5.Name = "btn_GIF5";
+            btn_GIF5.Size = new Size(42, 20);
+            btn_GIF5.Text = "5.gif";
+            btn_GIF5.Click += btn_gif5_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonFace;
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(168, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Exit";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ButtonFace;
+            button2.Location = new Point(87, 27);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Hide";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ButtonFace;
+            button3.Location = new Point(6, 27);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "Clear\r\n";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(249, 58);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(menuStrip2);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Menu";
+            Text = "Desktop Anime GIF";
+            TopMost = true;
+            FormClosing += Menu_FormClosing;
+            Load += Menu_Load;
+            menuStrip2.ResumeLayout(false);
+            menuStrip2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip2;
+        private ToolStripMenuItem btn_GIF2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ToolStripMenuItem btn_GIF3;
+        private ToolStripMenuItem btn_GIF1;
+        private ToolStripMenuItem btn_GIF4;
+        private ToolStripMenuItem btn_GIF5;
+    }
+}
---
+++ Forms/Menu.cs
@@ -0,0 +1,107 @@
+﻿using DesktopKonata.Utility;
+using System.Windows.Forms;
+
+namespace DesktopKonata.Forms
+{
+    public partial class Menu : Form
+    {
+        private readonly Screen[] AllScreens = Screen.AllScreens;
+        private readonly List<CharacterWindow> _windows = new();
+
+        private NotifyIcon _notifyIcon = new()
+        {
+            Visible = true,
+            Icon = Properties.Resources.KonataIco,
+            Text = "hehe",
+            BalloonTipIcon = ToolTipIcon.Info,
+            BalloonTipTitle = "hehe",
+            BalloonTipText = "I`am here!"
+        };
+
+        public Menu()
+        {
+            InitializeComponent();
+            _notifyIcon.MouseClick += NotifyIcon_MouseClick!;
+        }
+
+        private void Menu_Load(object sender, EventArgs args)
+        {
+            foreach (var screen in AllScreens)
+            {
+                var window = new CharacterWindow(screen);
+                window.SetBounds(screen.Bounds.X, screen.Bounds.Y, screen.Bounds.Width, screen.Bounds.Height);
+                _windows.Add(window);
+                window.Show();
+            }
+        }
+
+        private void HideInTray()
+        {
+            Hide();
+            _notifyIcon.ShowBalloonTip(500);
+        }
+
+        private void Menu_FormClosing(object sender, FormClosingEventArgs args)
+        {
+
+        }
+
+        private void NotifyIcon_MouseClick(object sender, MouseEventArgs args)
+        {
+            WindowState = FormWindowState.Normal;
+            Show();
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _notifyIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HideInTray();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CharacterManager.ClearCharacters();
+        }
+
+        private void btn_gif1_Click(object sender, EventArgs e)
+        {
+            Bitmap gifFromDisk = new Bitmap("GIF/1.gif");
+            CharacterManager.AddCharacter(gifFromDisk);
+        }
+
+        private void btn_gif2_Click(object sender, EventArgs e)
+        {
+            Bitmap gifFromDisk = new Bitmap("GIF/2.gif");
+            CharacterManager.AddCharacter(gifFromDisk);
+        }
+
+        private void btn_gif3_Click(object sender, EventArgs e)
+        {
+            Bitmap gifFromDisk = new Bitmap("GIF/3.gif");
+            CharacterManager.AddCharacter(gifFromDisk);
+        }
+
+        private void btn_gif4_Click(object sender, EventArgs e)
+        {
+            Bitmap gifFromDisk = new Bitmap("GIF/4.gif");
+            CharacterManager.AddCharacter(gifFromDisk);
+        }
+
+        private void btn_gif5_Click(object sender, EventArgs e)
+        {
+            Bitmap gifFromDisk = new Bitmap("GIF/5.gif");
+            CharacterManager.AddCharacter(gifFromDisk);
+            //CharacterManager.AddCharacter(Properties.Resources.GIF5); // BananaKonataGIF
+        }
+    }
+}
